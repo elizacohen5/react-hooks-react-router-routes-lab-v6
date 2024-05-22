6fddcd2aa6d57941d8a63188aae14304012d3c66
Eliza Cohen <elizacohen5@gmail.com>
2024-05-22 10:26:12 -0400

    all tests but one passing

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/actors`)
+      .then((r) => r.json())
+      .then((actors) => setActors(actors))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const actorArticle = (actors || []).map((actor) => {
+    const actorMovieList = actor.movies.map((movie) => {
+      return <li key={movie}>{movie}</li>;
+    });
+    return (
+      <article key={actor.id}>
+        <h2>{actor.name}</h2>
+        <ul>{actorMovieList}</ul>
+      </article>
+    );
+  });
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actorArticle}
       </main>
     </>
   );
-};
+}
 
 export default Actors;
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,33 @@
+import NavBar from "../components/NavBar";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
 
 function Movie() {
+
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+  const id = params.id
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/movies/${id}`)
+    .then(r => r.json())
+    .then(movie => setMovie(movie))
+    .catch(error => console.log(error));
+  }, [id])
+
+  const movieGenre = (movie.genres || []).map(genre => {
+    return <span key={genre}>{genre}</span>
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        <div>{movieGenre}</div>
       </main>
     </>
   );
